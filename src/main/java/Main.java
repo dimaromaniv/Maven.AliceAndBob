43ebeaa45826c55889be46c368eae5a2d8d0b2e0
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-06-29 13:19:57 -0400

    I have create a greeting program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello humman ! What is your name ? ");
+        String answerForQuestion ;
+        answerForQuestion = scanner.nextLine();
+        System.out.println("My greetings,  " + answerForQuestion);
 
     }
 }
